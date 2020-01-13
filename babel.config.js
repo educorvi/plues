d7206472b2650df0597b406f05bb39afd6891b7b
Lars Walther <meinkraeks@gmail.com>
2020-01-13 13:00:08 +0100

    Korrektur von babel.config.json

--- babel.config.js
+++ babel.config.js
@@ -2,17 +2,5 @@ module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
   ],
-  module: {
-    rules: [{
-      test: /\.scss$/,
-      use: [{
-        loader: "style-loader" // creates style nodes from JS strings
-      }, {
-        loader: "css-loader" // translates CSS into CommonJS
-      }, {
-        loader: "sass-loader" // compiles Sass to CSS
-      }]
-    }]
-  }
   // resolve: { alias: { vue: 'vue/dist/vue.esm.js' } }
 };
