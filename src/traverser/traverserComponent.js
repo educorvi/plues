315dd7913b9c21aab5148c777e2d2bdbd42ba8f3
Julian <neferin42@gmail.com>
2020-01-20 10:40:05 +0100

    Spinner while Traversing

--- src/traverser/traverserComponent.js
+++ src/traverser/traverserComponent.js
@@ -1,4 +1,7 @@
+// eslint-disable-next-line no-unused-vars
+import CustomSpinner from "@/components/Helper/CustomSpinner";
+
 export default {
     name: 'traverser-component',
-    template: '<p>traverser component...</p>',
+    template: '<custom-spinner></custom-spinner>',
 };
