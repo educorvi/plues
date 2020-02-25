b278b25d193aa2e79bc63fd4254cab385532124a
Julian <neferin42@gmail.com>
2020-02-25 10:26:18 +0100

    Corrected Styling

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,8 @@
 <template>
     <div ref="parentSidebar">
         <Breadcrumbs v-if="!$route.query.nonav"/>
-        <h1 v-if="config.sideBar">
-            <b-icon-list @click="$refs.sidebar.active = true" class="sidebarOpener"/>
-        </h1>
+            <b-icon-gear @click="$refs.sidebar.active = true" class="sidebarOpener" font-scale="2.4"
+                         v-if="config.sideBar"/>
         <traverser-view/>
         <Sidebar ref="sidebar" v-if="config.sideBar"/>
     </div>
@@ -15,12 +14,12 @@
     import Breadcrumbs from "@/components/Navigation/Breadcrumbs";
     import {mapGetters} from "vuex"
     import Sidebar from "@/views/Sidebar";
-    import {BIconList} from "bootstrap-vue"
+    import {BIconGear} from "bootstrap-vue"
 
     export default {
         /* eslint-disable no-console */
         name: 'home',
-        components: {Sidebar, Breadcrumbs, BIconList},
+        components: {Sidebar, Breadcrumbs, BIconGear},
 
         mounted() {
             this.traverse({"@id": this.$route.path + "?rel"});
@@ -38,10 +37,14 @@
         @extend .rounded;
         background-color: $primary;
         float: left;
-        /*position: sticky;*/
-        /*position: -webkit-sticky;*/
-        /*top: 80px;*/
-        /*left: 10px;*/
+        position: sticky;
+        position: -webkit-sticky;
+        top: 80px;
+        left: 10px;
+        @media (min-width: 992px) {
+            margin-left: -8%;
+            margin-top: -5%;
+        }
         color: $navbar-light-color;
         z-index: 1000;
 
