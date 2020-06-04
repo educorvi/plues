49c471fb13840a4457428e5e3b9155f685e50163
Neferin <neferin42@gmail.com>
2020-06-04 15:58:13 +0200

    Namen des Content Types wird in default View angezeigt

--- src/components/Traverser/_defaultView.vue
+++ src/components/Traverser/_defaultView.vue
@@ -8,7 +8,7 @@
             <template v-slot:header>Default View</template>
 
             <template v-slot:lead>
-                Für diesen Contenttype wurde noch keine Ansicht erstellt, deshalb wird diese Standardansicht angezeigt.
+                Für den Contenttype "{{context["@type"]}}" wurde noch keine Ansicht erstellt, deshalb wird diese Standardansicht angezeigt.
             </template>
         </b-jumbotron>
     </section>
